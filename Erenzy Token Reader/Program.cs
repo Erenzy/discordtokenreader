29eacd560c4ba92633d76c7fcf581c25b3cecc1a
Erenzy <77503420+erenzy1337@users.noreply.github.com>
2021-02-06 18:10:41 +0300

    Add files via upload

---
+++ Erenzy Token Reader/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Erenzy_Token_Reader
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            new DiscordToken();
+            Console.ReadLine();
+        }
+    }
+
+    public class DiscordToken
+    {
+        public DiscordToken()
+        {
+            GetToken();
+        }
+
+        public void GetToken()
+        {
+            Console.Title = "Erenzy Token Reader";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("Results will appear on tokens.txt");
+            var files = SearchForFile(); // to get ldb files
+            if (files.Count == 0)
+            {
+                Console.WriteLine("No .ldb file found!\nPlease put .ldb files same directory with token reader!");
+                return;
+            }
+            foreach (string token in files)
+            {
+                foreach (Match match in Regex.Matches(token, "[^\"]*"))
+                {
+                    if (match.Length == 59)
+                    {
+                        Console.WriteLine($"Token={match.ToString()}");
+                        using (StreamWriter sw = new StreamWriter("tokens.txt", true))
+                        {
+                            sw.WriteLine($"Token={match.ToString()}");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Done");
+        }
+
+        private List<string> SearchForFile()
+        {
+            List<string> ldbFiles = new List<string>();
+            string currentPath = Environment.CurrentDirectory;
+
+            if (!Directory.Exists(currentPath))
+            {
+                Console.WriteLine("404");
+                return ldbFiles;
+            }
+
+            foreach (string file in Directory.GetFiles(currentPath, "*.ldb", SearchOption.TopDirectoryOnly))
+            {
+                string rawText = File.ReadAllText(file);
+                if (rawText.Contains("oken"))
+                {
+                    Console.WriteLine($"{Path.GetFileName(file)} added");
+                    ldbFiles.Add(rawText);
+                }
+            }
+            return ldbFiles;
+        }
+    }
+}
---
+++ Erenzy Token Reader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Erenzy Token Reader")]
+[assembly: AssemblyDescription("Erenzy#1337")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Erenzy Token Reader")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d3af5610-f0e6-4634-a210-5d2d0ba560e0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.3.7")]
+[assembly: AssemblyFileVersion("1.3.3.7")]
